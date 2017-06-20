7de2d6eee7540edbe33f762e77397d8a5e23b5a0
Lino Silva <lino.silva@farfetch.com>
2017-06-20 22:38:00 +0100

    Added WiFiUdp in preparation for Bonjour

--- src/main.ino
+++ src/main.ino
@@ -1,8 +1,11 @@
 #include <Arduino.h>
 #include <ArduinoOTA.h>
-#include <ESP8266WebServer.h>
+
 #include <ESP8266mDNS.h>
+#include <ESP8266WebServer.h>
 #include <ESP8266WiFi.h>
+
+#include <WiFiUdp.h>
 
 const uint DEBUG = 1;
 
