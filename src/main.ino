2d80dd60a0c9633205f25a2f95aa38a5740618b4
Lino Silva <lino.silva@farfetch.com>
2017-06-20 22:50:31 +0100

    Added advertising, conditional debugging

--- src/main.ino
+++ src/main.ino
@@ -13,6 +13,9 @@ const uint MAX_WIFI_CONNECT_TRY = 60;
 const char* WLAN_SSID = "<redacted>";
 const char* WLAN_PWD = "<redacted>";
 
+const uint OTA_PORT = 0;
+const char* OTA_PASSWORD = "<redacted>";
+
 ESP8266WebServer webServer(80);
 
 bool OTAUpdate = false;
@@ -23,85 +26,132 @@ void setup_wifi() {
   int xTry = 0;
   delay(10);
 
+#if defined(DEBUG)
   Serial.println();
   Serial.print("Connecting to ");
   Serial.println(WLAN_SSID);
+#endif
 
   WiFi.begin(WLAN_SSID, WLAN_PWD);
 
   while (WiFi.status() != WL_CONNECTED && ++xTry < MAX_WIFI_CONNECT_TRY) {
     delay(500);
-    Serial.print(".");
+#if defined(DEBUG)
+      Serial.print(".");
+#endif
   }
 
   if (WiFi.status() != WL_CONNECTED){
-    Serial.println("Wifi Failed.. Restarting");
+#if defined(DEBUG)
+      Serial.println("Wifi Failed.. Restarting");
+#endif
     delay(100);
     ESP.restart();
   }
 
-  Serial.println("");
-  Serial.println("WiFi connected");
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
+#if defined(DEBUG)
+    Serial.println("");
+    Serial.println("WiFi connected");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+#endif
 }
 
 void handleOTARoot() {
-  if (DEBUG) Serial.println("GOT OTA cmd");
+#if defined(DEBUG)
+    Serial.println("GOT OTA cmd");
+#endif
   OTAUpdate = true;
   OTAend = millis()+OTATimeout;
   webServer.send(200, "text/plain", "GO");
-  if (DEBUG) Serial.println("Start OTA");
+#if defined(DEBUG)
+    Serial.println("Start OTA");
+#endif
 }
 
 void bindOTAEvents(){
   ArduinoOTA.onStart([]() {
 
-    if (DEBUG) Serial.println("Start updating ");
+#if defined(DEBUG)
+    Serial.println("Start updating ");
+#endif
   });
 
   ArduinoOTA.onEnd([]() {
-    if (DEBUG) Serial.println("\nEnd");
+#if defined(DEBUG)
+    Serial.println("\nEnd");
+#endif
     OTAUpdate =false;
     delay(100);
     ESP.restart();
   });
 
   ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
-    if (DEBUG) Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  #if defined(DEBUG)
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  #endif
   });
 
   ArduinoOTA.onError([](ota_error_t error) {
-    if (DEBUG) {
-      Serial.printf("Error[%u]: ", error);
-      if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
-      else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
-      else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
-      else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
-      else if (error == OTA_END_ERROR) Serial.println("End Failed");
-    }
+#if defined(DEBUG)
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+    else if (error == OTA_END_ERROR) Serial.println("End Failed");
+#endif
 
     delay(100);
     ESP.restart();
   });
 
-  ArduinoOTA.setPort(9069);
-  ArduinoOTA.setPassword("M4yTh3F0rc3B3W1thY0u");
+  ArduinoOTA.setPort(OTA_PORT);
+  ArduinoOTA.setPassword(OTA_PASSWORD);
   ArduinoOTA.begin();
 }
 
-void setup() {
-  if (DEBUG) Serial.begin(115200);
+void setup_mdns() {
 
+  // Set up mDNS responder:
+  // - first argument is the domain name, in this example
+  //   the fully-qualified domain name is "esp8266.local"
+  // - second argument is the IP address to advertise
+  //   we send our IP address on the WiFi network
+  if (!MDNS.begin("esp8266")) {
+
+#if defined(DEBUG)
+    Serial.println("Error setting up MDNS responder!");
+#endif
+
+    while(1) {
+      delay(1000);
+    }
+  }
+
+#if defined(DEBUG)
+  Serial.println("mDNS responder started");
+#endif
+}
+
+void setup() {
+#if defined(DEBUG)
+  Serial.begin(115200);
+#endif
   setup_wifi();
+  setup_mdns();
   webServer.on("/update", handleOTARoot);
   webServer.begin();
-  if (DEBUG) Serial.println("HTTP server started");
+#if defined(DEBUG)
+  Serial.println("HTTP server started");
+#endif
   bindOTAEvents();
+  // Add service to MDNS-SD
+  MDNS.addService("http", "tcp", 80);
 }
 
 void loop(){
-   if (OTAUpdate && millis() - OTAend>0) {
+   if (OTAUpdate && millis() - OTAend > 0) {
 
      ArduinoOTA.handle();
    } else {
